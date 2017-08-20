8b8e0697e09498993e29bcc46b1938a17c35171c
Aleks Kvetko <aleks.kvetko@gmail.com>
2017-08-20 17:20:39 +0300

    updates

--- src/main/java/by/alekskvetko/heroku_test/Home.java
+++ src/main/java/by/alekskvetko/heroku_test/Home.java
@@ -1,13 +1,29 @@
 package by.alekskvetko.heroku_test;
 
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 public class Home {
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "Hello world!";
+    @RequestMapping(value = "{name}", method = RequestMethod.GET)
+    public String hello(@PathVariable("name") String name) {
+        if (!name.equals("secret")) {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+            String time = sdf.format(new Date());
+            return String.format("Hi, %s, current time is %s", name, time);
+        } else {
+            String secret = System.getenv("secret");
+            if (StringUtils.isEmpty(secret)) {
+                return "There is no secret message...";
+            } else {
+                return secret;
+            }
+        }
     }
 }
