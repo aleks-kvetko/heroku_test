4f40d65f675dafa71173857e6c3cf782901c215c
Aleks Kvetko <aleks.kvetko@gmail.com>
2017-08-20 16:52:55 +0300

    init

---
+++ src/main/java/by/alekskvetko/heroku_test/Home.java
@@ -0,0 +1,13 @@
+package by.alekskvetko.heroku_test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello world!";
+    }
+}
